b404c9601b1be1b4bf45372cf9a8b55c9cd3d88d
Israel Castro <israel.castro@gmail.com>
2020-05-08 10:18:27 -0500

    Programacion orientada a objetos

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $limitMonths = 2000;
             <?php
             $totalMonths = 0;
             for ($idx = 0; $idx < count($jobs); $idx++) {
-              $totalMonths += $jobs[$idx]['months'];
+              $totalMonths += $jobs[$idx]->months;
               
               if ($totalMonths > $limitMonths) {
                 break;
--- jobs.php
+++ jobs.php
@@ -1,36 +1,65 @@
 <?php
 
+class Job {
+    private $title;
+    public $description;
+    public $visible;
+    public $months;
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+}
+
+$job1 = new Job();
+$job1->setTitle('PHP Developer');
+$job1->description = 'This is an awesome job!!!';
+$job1->visible = true;
+$job1->months = 16;
+
+$job2 = new Job();
+$job2->setTitle('Python Developer');
+$job2->description = 'This is an awesome job!!!';
+$job2->visible = true;
+$job2->months = 24;
+
 $jobs = [
-    [
-      'title' => 'PHP Developer',
-      'description' => 'This is an awesome job!!!',
-      'visible' => true,
-      'months' => 16
-    ],
-    [
-      'title' => 'Python Deb',
-      'description' => '',
-      'visible' => false,
-      'months' => 14
-    ],
-    [
-      'title' => 'Devops',
-      'description' => '',
-      'visible' => true,
-      'months' => 5
-    ],
-    [
-      'title' => 'Node Dev',
-      'description' => '',
-      'visible' => true,
-      'months' => 24
-    ],
-    [
-      'title' => 'Frontend Dev',
-      'description' => '',
-      'visible' => true,
-      'months' => 3
-    ]
+    $job1,
+    $job2
+    // [
+    //   'title' => 
+    //   'description' => 
+    //   'visible' => 
+    //   'months' => 
+    // ],
+    // [
+    //   'title' => 'Python Deb',
+    //   'description' => '',
+    //   'visible' => false,
+    //   'months' => 14
+    // ],
+    // [
+    //   'title' => 'Devops',
+    //   'description' => '',
+    //   'visible' => true,
+    //   'months' => 5
+    // ],
+    // [
+    //   'title' => 'Node Dev',
+    //   'description' => '',
+    //   'visible' => true,
+    //   'months' => 24
+    // ],
+    // [
+    //   'title' => 'Frontend Dev',
+    //   'description' => '',
+    //   'visible' => true,
+    //   'months' => 3
+    // ]
 ];
 
 function getDuration($months) {
@@ -47,14 +76,14 @@ if ($years > 0 && $extraMonts > 0) {
 }
 
 function printJob($job){
-if ($job['visible'] == false) {
+if ($job->visible == false) {
     return;
 }
 
 echo '<li class="work-position">';
-echo '<h5>' . $job['title'] . '</h5>';
-echo '<p>' . $job['description'] . '</p>';
-echo '<p>' . getDuration($job['months']) . '</p>';
+echo '<h5>' . $job->getTitle() . '</h5>';
+echo '<p>' . $job->description . '</p>';
+echo '<p>' . getDuration($job->months) . '</p>';
 echo '<strong>Achievements:</strong>';
 echo '<ul>';
 echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
